542fe722c46ad2d5b07c632bb3bf4859ca291299
Jesper Lundberg <lundberg.jesper@gmail.com>
2024-06-07 23:54:26 +0200

    Fix infite loop

--- lua/svartafanan/scramble.lua
+++ lua/svartafanan/scramble.lua
@@ -3,19 +3,16 @@ local file = require("svartafanan.file")
 local M = {}
 
 local function get_random_move(full_scramble_notation, previous_move)
-	-- Set the seed for the random number generator
-	math.randomseed(os.time())
-
-	-- Generate a random number between 1 and the length of the full scramble notation
-	local random_number = math.random(1, #full_scramble_notation)
+	local random_number
 	local random_move
 
-	-- Get the random move and make sure it's not the same as the previous move
-	-- FIXME: This creates an infinite loop if the previous move is the same as the random move or if the previous move is nil
-	while previous_move == nil or string.sub(random_move, 1, 1) == string.sub(previous_move, 1, 1) do
-		-- Generate a new random number
+	repeat
+		-- Generate a random number between 1 and the length of the full scramble notation
+		random_number = math.random(1, #full_scramble_notation)
+
+		-- Get the random move
 		random_move = full_scramble_notation[random_number]
-	end
+	until string.sub(random_move, 1, 1) ~= string.sub(previous_move, 1, 1)
 
 	-- Return the random move
 	return random_move
@@ -25,16 +22,20 @@ function M.scramble(cube_size)
 	-- Read the scramble file based on the cube size
 	local file_contents = file.read(cube_size)
 
-	-- The first move is always added as it's always valid
-	local scramble = { get_random_move(file_contents.availableMoves) }
+	-- Set the seed for the random number generator
+	math.randomseed(os.time())
 
-	print(vim.inspect(scramble))
+	-- The first move is always added as it's always valid
+	local scramble = { get_random_move(file_contents.availableMoves, "num1") }
 
 	-- Start at 2 since the first move is already added
 	for i = 2, file_contents.numberOfMoves do
-		print("i: " .. i)
-		table.insert(scramble, get_random_move(file_contents.availableMoves, scramble[i - 1]))
+		local previous_move = scramble[i - 1]
+
+		table.insert(scramble, get_random_move(file_contents.availableMoves, previous_move))
 	end
+
+	print(vim.inspect(scramble))
 
 	return scramble
 end
